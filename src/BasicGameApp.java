b12a38205aa7970e95517aa574b019d4d0aec341
sheaeubanks27-arch <shea_eubanks27@milton.edu>
2026-05-07 10:57:37 -0400

    Started suit and basic game app constructor

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -4,8 +4,22 @@ public class BasicGameApp {
     public Dealer d;
 
     public static void main(String[] args) {
+        new BasicGameApp();
 
     }
+
+    public BasicGameApp(){
+        System.out.println("welcome to Blackjack");
+        deck = new Card[52];
+        //todo: fill the array deck with your favorite card
+
+        for(int x= 0; x<13; x++){
+            deck[x] = new Card("hearts",10,x);
+            deck[x].printInfo();
+        }
+    }
+
+    //todo: make a printDeck method that prints the deck with printinfo
 
     public void compare(){
 
--- src/Card.java
+++ src/Card.java
@@ -2,4 +2,32 @@ public class Card {
     String suit;
     int value;
     String name;
+
+    //todo: make a constructor that has three parameters, one for each variable
+    //todo: set the instance variables according to the parameters
+
+    public Card(String pSuit, int pValue, int pName){
+        suit = pSuit;
+        value = pValue;
+        if(pName == 0){
+            name = "Ace";
+            value = 11;
+        }
+        else if(pName == 1){
+            name = "two";
+            value = 2;
+            //todo: make the whole suit
+        }
+       // name = pName;
+
+    }
+
+    //todo: make a print info that prints the info of the card
+    //the ___ of ___ is worth ___ points
+
+
+    public void printInfo(){
+        System.out.println("the " + name +" of " + suit + " is worth " + value + " points.");
+    }
+
 }
