7edb6a8e9ebaea12ab9efc82f647ddd300d5fdef
sheaeubanks27-arch <shea_eubanks27@milton.edu>
2026-05-13 23:05:03 -0400

    Homework

--- src/Dealer.java
+++ src/Dealer.java
@@ -4,6 +4,13 @@ public class Dealer {
     boolean isBust;
     Card[]hand;
 
+    public Dealer() {
+        isOver15 = false;
+        cardTotal = 12;
+        isBust = true;
+        hand = new Card[2];
+    }
+
     public void hit(){
 
     }
@@ -11,5 +18,12 @@ public class Dealer {
     public void stand(){
 
 
+    }
+
+    public void printInfo(){
+        System.out.println("It is " + isOver15 +" that the card value is over 15 ");
+        System.out.println("The card total is: " + cardTotal);
+        System.out.println("It is " + isBust + " that the hand has busted");
+        System.out.println("The hand is: " + hand);
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -6,11 +6,21 @@ public class Player {
     public  String name;
 
     public Player(){
-        cardTotal = 2;
+        cardTotal = 16;
         isBust = false;
         isHit = true;
         name = "shea";
+        hand = new Card[3];
     }
+
+    public void printInfo(){
+        System.out.println("The card total is: " + cardTotal);
+        System.out.println("It is " + isHit +" that the player has hit ");
+        System.out.println("The card total is: " + cardTotal);
+        System.out.println("It is " + isBust + " that the hand has busted");
+        System.out.println("The hand is: " + hand);
+    }
+
     public void hit(){
 
     }
